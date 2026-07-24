8055324c9716311a8142cb6334f8aa46663e7550
vivek <vivekchinthala3@gmail.com>
2026-07-24 16:15:14 +0530

    added  sprintboot

---
+++ practice01/src/test/java/com/vivek/practice01/Practice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.practice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
